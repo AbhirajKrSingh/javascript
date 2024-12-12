07d6a0ea4538e280a255175f466def8946be9907
AbhirajKrSingh <avirajsingh176@gmail.com>
2024-12-12 06:21:48 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+ "use strict"; // treat all JS code as newer version
+
+//  alert(3+3)  // we are using nodejs, not browser
+
+console.log(3   // code readabilty should be high
+    +3
+)
+console.log("Hello")
+
+let name = "Abhiraj"
+let age = 18
+let isLogedIn = false
+let state;    // this is undefined because variable is declared but value is not given
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean=> true/false
+// null => standalone value    // this mean wo empty hai
+// undefined => abhi value assign nhi hui hai
+// symbol => use when we are talking about unique
+
+// object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);   // object         // interview question....
+console.log(typeof undefined); // undefined
+
