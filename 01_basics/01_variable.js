967926057409ee7866943d542dcc17871c9d5c55
AbhirajKrSingh <avirajsingh176@gmail.com>
2024-12-12 06:22:08 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 27837268
+let accountEmail = "aviraj@gmail.com"
+var accountPassward = "12345"
+accountCity = "jaipur"
+let accountState
+
+// accountId = 2  :- not changable
+
+accountEmail = "av@av.com"
+accountPassward ="78957985"
+accountCity = "udaipur"
+
+
+/*
+prefer not to use var
+becaue of issue in block scop and funtional scop
+*/
+
+
+console.log(accountId);
+
+console.table([ accountId, accountEmail, accountPassward,accountCity,accountState])
