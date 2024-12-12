134512d171cb91105fa6cf962258b160f7f3de9b
AbhirajKrSingh <avirajsingh176@gmail.com>
2024-12-12 07:00:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = 33
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);   // number
+// console.log(valueInNumber); // NaN   this is issue of JS
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// true => 1
+// false => 0
+// undefined => NaN
+
+let isLogedIn = "hello"
+let booleanIsLogedIn = Boolean(isLogedIn)
+// console.log(booleanIsLogedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hello" => true
+
+let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
